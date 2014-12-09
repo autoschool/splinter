93fb343f09807c318d8532fd0fe14304098d0b94
Etki <etki@etki.name>
2014-12-09 22:56:58 +0300

    Added application class scaffolding

--- src/main/resources/db/migration/flyway.backup/V1__posts.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE posts (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(128),
-    content VARCHAR(65000), -- 65535 is the whole row size for MySQL
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP
-);
